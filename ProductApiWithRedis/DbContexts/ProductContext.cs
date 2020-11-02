95dce3bf8da6aa9b63f039530491764d0100288a
Kamron Phray <alohaphray@gmail.com>
2020-11-02 11:27:35 +0500

    CW first commit

---
+++ ProductApiWithRedis/DbContexts/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApiWithRedis.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApiWithRedis.DbContexts
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ProductApiWithRedis/Repository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductApiWithRedis.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApiWithRedis.Repository
+{
+    public interface IProductRepository
+    {
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int productId);
+        Product GetProductById(int Id);
+        IEnumerable<Product> GetProducts();
+    }
+}
